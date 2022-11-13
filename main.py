319602ad5228369e576ec3f8223db94103bc0c21
Laplas00 <20helllucifer02@gmail.com>
2022-11-13 19:03:13 +0200

    check this
    nu vrode kak

--- main.py
+++ main.py
@@ -24,7 +24,6 @@ def hello_handler():
 
 
 
-
 def quit_handler():
     print("Good bye!")
     CONTACTS.save_contacts()
@@ -47,15 +46,17 @@ def add_contact_handler(var):
 @input_error
 def find_contact_handler(var):
     for name, record in CONTACTS.items():
-        if name == var.split()[1]:
+        if name == var.split()[0]:
             print(
                 f"{name.capitalize()}: {[phone.value for phone in record.phones]}")
+        else:
+            print('no')
 
 
 @input_error
 def delete_contact_handler(var):
-    name = var.split()[2]
-    phone_for_delete = var.split()[3]
+    name = var.split()[0]
+    phone_for_delete = var.split()[1]
     record = CONTACTS.data[name]
     record.delete_phone(phone_for_delete)
     print("Contact's phone was deleted")
@@ -63,9 +64,9 @@ def delete_contact_handler(var):
 
 @input_error
 def change_contact_handler(var):
-    name = var.split()[1]
-    phone_for_change = var.split()[2]
-    new_phone = var.split()[3]
+    name = var.split()[0]
+    phone_for_change = var.split()[1]
+    new_phone = var.split()[2]
     if phone_for_change and new_phone:
         record = CONTACTS.data[name]
         record.change_phone(phone_for_change, new_phone)
@@ -74,8 +75,8 @@ def change_contact_handler(var):
 
 @input_error
 def add_birthday_handler(var):
-    name = var.split()[2]
-    birthday = var.split()[3]
+    name = var.split()[0]
+    birthday = var.split()[1]
     if name in CONTACTS:
         record = CONTACTS.data[name]
         if record.birthday == "":
@@ -83,6 +84,8 @@ def add_birthday_handler(var):
             print("Contact's birthday was added")
         else:
             print("Contact's birthday was added before")
+    else:
+        print('This contact dont exist')
 
 
 @input_error
@@ -154,13 +157,15 @@ def clean_folder():
 
 @input_error
 def add_note_handler(var):
-    name = var.split()[1]
-    note = " ".join(var.split()[2:])
+    name = var.split()[0]
+    note = " ".join(var.split()[1:])
     if name in CONTACTS:
         record = CONTACTS.data[name]
         if record.note == "":
             record.add_note(note)
             print("Contact's note was added")
+    else:
+        print('name dont exist')
 
 
 def show_notes_handler():
@@ -176,8 +181,8 @@ def show_notes_handler():
 
 @input_error
 def add_tag_handler(var):
-    name = var.split()[1]
-    tag = " ".join(var.split()[2:])
+    name = var.split()[0]
+    tag = " ".join(var.split()[1:])
     if name in CONTACTS:
         record = CONTACTS.data[name]
         if record.note != "":
@@ -190,6 +195,8 @@ def add_tag_handler(var):
 def show_tags_handler():
     show_list = []
     for name, record in CONTACTS.items():
+        print(name, record)
+        print(record.tag)
         if record.tag != {}:
             show_list.append(record.tag)
     if show_list != []:
@@ -201,7 +208,7 @@ def show_tags_handler():
 
 @input_error
 def delete_note_handler(var):
-    name = var.split()[2]
+    name = var.split()[0]
     record = CONTACTS.data[name]
     record.note = ""
     record.tag = {}
@@ -247,7 +254,7 @@ def find_notes(var):
 
 @input_error
 def add_address_handler(var):
-    name = var.split()[2]
+    name = var.split()[0]
     address = " ".join(var.split()[3:])
     if name in CONTACTS:
         record = CONTACTS.data[name]
@@ -258,8 +265,8 @@ def add_address_handler(var):
             print("Contact's address was added before")
 
 
-@input_error
-def find_address_handler(var):
+
+def find_address_handler():
     show_list = []
     for name, record in CONTACTS.items():
         if record.address != "":
@@ -274,15 +281,15 @@ def find_address_handler(var):
 
 @input_error
 def show_list_birthday_handler(var):
-    interval = int(var.split()[1])
+    interval = int(var.split()[0])
     for record in CONTACTS.values():
         record.interval_birthday(interval)
 
 
 @input_error
 def add_email_handler(var):  # ____________________________email
-    name = var.split()[2]
-    email = var.split()[3]
+    name = var.split()[0]
+    email = var.split()[1]
     if name in CONTACTS:
         record = CONTACTS.data[name]
         if record.email == "":
@@ -306,30 +313,29 @@ def show_email_handler():  # ____________________________email
 
 COMMANDS = {
      "hello": [hello_handler, 'show commands'],
-     "all tags": [show_tags_handler, 'show all tags'],
-     "all notes": [show_notes_handler, 'show all notes'],
      "add": [add_contact_handler, '[name] [phone]'],
      "add birthday" : [add_birthday_handler, '[name] [dd.mm.yyyy]'],
-     "add address" : [add_address_handler, '???'],
+     "add address" : [add_address_handler, '[name] [adress]'],
+     "add note" : [add_note_handler, '[name] [note]'],
+     "add tag" : [add_tag_handler, '[name] [tag]'],
      "change note" : [change_note_handler, '[name] [new_note]'],
-     "adress": [find_address_handler,'???'],
-     "birthday" : [days_to_birthday_handler, '[name] days until birthday'],
-     "show all": [show_contacts_handler, 'show all contacts'],  
-     "all email": [show_email_handler, 'show all emails'],
-     "iter": [iteration, 'iteration with all notes'],
-     "sort": [clean_folder,'???'],
-     "change" : [change_contact_handler,'[name] [phone] [new_phon]e'],
-     "phone" : [find_contact_handler, '[name]'],
-     "delete phone" : [delete_contact_handler, '[name] [number]'],
-     "note" : [add_note_handler, '[name] [note]'],
-     "tag" : [add_tag_handler, '[name] [tag]'],
      "delete note" : [delete_note_handler, '[name]'],
-     "find tag" : [find_tag_handler, '[tag_name]']}
+     "change phone" : [change_contact_handler,'[name] [phone] [new_phone]'],
+     "delete phone" : [delete_contact_handler, '[name] [number]'],
+     "find phone" : [find_contact_handler, ' -- [name]'],
+     "find tag" : [find_tag_handler, '[tag_name]'],
+     "all tags": [show_tags_handler, 'show all tags'],
+     "all notes": [show_notes_handler, 'show all notes'],
+     "all email": [show_email_handler, 'show all emails'],
+     "show all": [show_contacts_handler, 'show all contacts'],  
+     "days before birthday" : [days_to_birthday_handler, '[name]'],
+     "iter": [iteration, 'iteration with all notes'],
+     "adress": [find_address_handler,'All adress'],
+     "sort": [clean_folder,'???'],
+}
 
 
 def main():
-
-
 
     while True:
         var = (input("Enter command: ")).lower().strip()
